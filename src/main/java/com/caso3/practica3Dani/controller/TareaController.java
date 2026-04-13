d94407236946fbe6296383168c4a30ce20dce0f3
Jarro <da.picarte@duocuc.cl>
2026-04-13 15:35:26 -0400

    post ok

--- src/main/java/com/caso3/practica3Dani/controller/TareaController.java
+++ src/main/java/com/caso3/practica3Dani/controller/TareaController.java
@@ -3,13 +3,20 @@ package com.caso3.practica3Dani.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.caso3.practica3Dani.model.Tarea;
 import com.caso3.practica3Dani.service.TareaService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -27,6 +34,14 @@ public class TareaController {
             .body("No hay tareas ingresadas");
         }
         return ResponseEntity.ok(lista);
+    }
+    @PostMapping
+    public ResponseEntity<?> postTarea(@Valid @RequestBody Tarea tarea){
+        Tarea t = tareaService.create(tarea);
+        if(t!=null){
+            return ResponseEntity.status(HttpStatus.CREATED).body("Tarea Creada.");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La tarea no se ha creado.");
     }
     
     
